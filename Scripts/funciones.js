1be2a06267b311ac8169f6e83e46b276b64e4ddb
Fernando Lo tartaro <flotartaroGit@gmail.com>
2021-12-26 12:40:50 -0300

    Masseuse@

---
+++ Scripts/funciones.js
@@ -0,0 +1,173 @@
+function cargarEncabezado(){
+	$("#tituloPrincipal").append("<p>Veronica D. Gutierrez</p>");
+	$("#descripcion").append("<p>Masajista especializada en tecnicas manuales descontracturantes y deportivas</p>");
+}
+
+
+function cargarInfoMasaje() {
+$("#infoMasaje").append("<h3 class='titulo'>Servicios</h3");	
+$("#infoMasaje").append("<h3 class='subtitulo'>Masoterapia</h3");
+$("#infoMasaje").append("<div id='contImg01_contenedor'></div>");
+$("#contImg01_contenedor").append("<img id='Img_masoterapia' src='Imagenes_vero/masoterapia.jpg'>");		
+$("#infoMasaje").append("<p>El masaje descontracturante es uno de los mas pedidos y utilizados para bajar los niveles de tension, producto del estres, movimientos repetititvos, adquisicion de malas posturas en el diario vivir que, a largo plazo, provocan dolencias y malestar corporal general</p>");	
+$("#infoMasaje").append("<p>Durante el proceso del masaje, el paciente siente un gran alivio y confort en las zonas mas afectadas, dando especial enfasis en cuello, espalda, zona lumbar, sin olvidar tambien piernas, muslos y gemelos, aplicando diferentes maniobras, tecnicas y presiones especificas, identificando aquella musculatura mas tensa y cansada, ayudandola a recuperar su funcion y movilidad normal.</p>");
+$("#infoMasaje").append("<h3 class='subtitulo'>Reflexologia Facial</h3");
+$("#infoMasaje").append("<div id='contImg02_contenedor'></div>");
+$("#contImg02_contenedor").append("<img id='Img_masoterapia' src='Imagenes_vero/reflexFacial.jpg'>");		
+$("#infoMasaje").append("<p>En nuestro rostro tenemos más de 500 puntos que están interconectados con  todo el cuerpo. La reflexología facial es una terapia introducida por Lone Sorensen y que se practica desde hace muchos años; permite que al tratar estos puntos se actúe sobre la totalidad del sistema nervioso.</p>");
+$("#infoMasaje").append("<p>Lore Sorensen es una técnica moderna que tiene sus raíces en la medicina oriental, y la finalidad es tratar de solucionar la causa de la enfermedad. O sea que en lugar de tratar los síntomas, se analizan y se tratan los desequilibrios orgánicos, físicos y emocionales, que normalmente son los causantes de diversos malestares.</p>");
+$("#infoMasaje").append("<h3 class='subtitulo'>Reflexologia Podal</h3");
+$("#infoMasaje").append("<div id='contImg03_contenedor'></div>");
+$("#contImg03_contenedor").append("<img id='Img_masoterapia' src='Imagenes_vero/reflexPodal.jpg'>");	
+$("#infoMasaje").append("<p>Un buen masaje de reflexología podal es una fuerza curativa increíble que puede ayudar a aliviar muchos síntomas tales como dolor de cabeza, insomnio, fatiga crónica, vértigos, así como ayuda a tratar la causa subyacente de muchas enfermedades internas tales como enfermedad hepática, enfermedad de riñón, Presión arterial alta, más virtualmente cualquier enfermedad conocida por el hombre.</p>");
+$("#infoMasaje").append("<p>También es una de las experiencias más agradables que se pueden tener, si se utilizan las técnicas adecuadas de masajes en los pies de reflexología.a causa de la enfermedad. O sea que en lugar de tratar los síntomas, se analizan y se tratan los desequilibrios orgánicos, físicos y emocionales, que normalmente son los causantes de diversos malestares.</p>");
+}
+
+
+function cargarTituloTurnos(){
+	$(".cargarTituloTurnos").append("TURNOS ONLINE");
+}
+
+
+function cargarBeneficiosRiesgos(){
+	$("#beneficiosRiesgos").append("<h3 class='titulo'>Beneficios y Riesgos</h3>");
+	$("#beneficiosRiesgos").append("<p>Un masaje terapéutico es la manipulación de los músculos y sus tendones, ligamentos y otros tejidos conectores, aplicando presión en el cuerpo. Los científicos no saben exactamente por qué la terapia de masaje funciona bien para tanta gente. Sin embargo, hay evidencia de razones físicas, fisiológicas y sicológicas de su éxito.</p>");
+	$("#beneficiosRiesgos").append("<p>Razones físicas. La presión en la piel puede aflojar músculos rígidos y tejido conector. El calor puede también aflojar músculos rígidos. Cierto tipo de técnica de masaje terapéutico usa almohadillas calientes y fricción rápida con las manos para calentar los músculos.</p>");
+	$("#beneficiosRiesgos").append("<p>Razones fisiológicas. La terapia de masaje puede ayudar a que el cuerpo libere ciertos químicos. Estos químicos ayudan a relajarnos, mejoran la circulación de la sangre y se deshacen de sustancias tóxicas que se acumulan en nuestros músculos a medida que éstos trabajan.</p>");
+	$("#beneficiosRiesgos").append("<p>Razones sicológicas. Muchas personas que reciben masaje terapéuticos se sienten mejor atendidas debido al tacto y al contacto con el terapista.</p>");
+	$("#beneficiosRiesgos").append("<p>El relajamiento con masaje terapéutico puede mejorar el sistema inmunológico. El sistema inmunológico del cuerpo combate los gérmenes y las enfermedades. Cuando el cuerpo se estresa, el sistema inmunológico está en menor capacidad de combatir enfermedades. Hay muchos tipos de terapia de masaje. Cada uno se basa en diferentes técnicas y algunos incluyen un componente espiritual. Este programa educativo no explica todos los diferentes tipos de masaje terapéutico.</p>");
+	$("#beneficiosRiesgos").append("<h3 class='subtitulo'>Beneficios</h3");
+	$("#beneficiosRiesgos").append("<ul style='list-style-type:disc' id='listaBeneficios'></ul>");
+	$("#listaBeneficios").append("<li>Favorece la relajacion fisica y psiquica.</li>");
+	$("#listaBeneficios").append("<li>Relaja los musculos.</li>");
+	$("#listaBeneficios").append("<li>Posee efectos calmantes del dolor.</li>");
+	$("#listaBeneficios").append("<li>Previene estados de estres, ansiedad e insomnio.</li>");
+	$("#listaBeneficios").append("<li>Mejora la autoestima.</li>");
+	$("#listaBeneficios").append("<li>Evita el envejecimiento.</li>");
+	$("#listaBeneficios").append("<li>Mejora la actividad de los organos internos.</li>");	
+	$("#listaBeneficios").append("<li>Ayuda a eliminar los desechos organicos.</li>");
+	$("#listaBeneficios").append("<li>Facilita la absorcion de liquidos previniendo la hinchazon.</li>");
+	$("#listaBeneficios").append("<li>Disuelve los cumulos de grasa, previniendo la celulitis.</li>");
+	$("#beneficiosRiesgos").append("<img id='Img_relax' src='Imagenes_vero/relaxation02.jpg'>");	
+	$("#beneficiosRiesgos").append("<h3 class='subtitulo'>Riesgos</h3");
+	$("#beneficiosRiesgos").append("<ul style='list-style-type:disc' id='listaRiesgos'></ul>");
+	$("#listaRiesgos").append("<li>Si usted padece una enfermedad de los vasos sanguíneos, consulte a su médico antes de recibir un masaje terapéutico. Si hay un coágulo en una vena, la terapia de masaje puede hacer que el coágulo se mueva.</li>");
+	$("#listaRiesgos").append("<li>Si usted padece una enfermedad cardiovascular, también debe consultar a su médico antes de recibir un masaje. Las enfermedades cardiovasculares incluyen vasos sanguíneos inflamados, coágulos de sangre y afecciones cardiacas.</li>");
+	$("#listaRiesgos").append("<li>Si usted tiene dolor de espalda severo, debe consultar a su médico antes de recibir un masaje terapéutico.</li>");
+	$("#listaRiesgos").append("<li>Por lo general, durante los primeros tres meses de embarazo no se debe masajear el abdomen, las piernas ni los pies. Es en este período cuando es más alto el riesgo de aborto. Sin embargo, un terapista de masaje con entrenamiento especializado en masaje terapéutico prenatal puede estar en capacidad de dar un masaje en los pies o en la parte baja de las piernas. Si usted está embarazada, consúltelo primero con su médico.</li>");
+	$("#listaRiesgos").append("<li>Si usted se rompió un hueso y éste no ha sanado, el área comprometida no debe masajearse. Generalmente, las personas que sufren de huesos frágiles, una enfermedad llamada osteoporosis, deben evitar la terapia de masaje. Sin embargo, aquellos con osteoporosis leve como resultado natural del envejecimiento pueden aún beneficiarse de un masaje terapéutico. Si usted sufre de osteoporosis, consulte a su médico antes de recibir terapia de masaje.</li>");
+	
+}
+
+
+function cargarTablaTarifas(){
+	$("#horariosTarifas").append("<h3 class='titulo'>Tarifas</h3>");
+	$("#horariosTarifas").append("<div class='tarjetaInst'></div>");
+	$(".tarjetaInst").append("<table id='tablaTarifas' border='1'></table>");
+	$("#tablaTarifas").append("<tr>");
+	$("#tablaTarifas").append("<th>Servicio</th>");
+	$("#tablaTarifas").append("<th>Duracion</th>");
+	$("#tablaTarifas").append("<th>Tarifa</th>");
+	$("#tablaTarifas").append("</tr>");
+	$("#tablaTarifas").append("<tr>");
+	$("#tablaTarifas").append("<td>Masaje descontracturante + reflexología (Facial o Podal)</td>");
+	$("#tablaTarifas").append("<td>1 hora y 15 minutos</td>");
+	$("#tablaTarifas").append("<td>$450</td>");
+	$("#tablaTarifas").append("</tr>");
+	$("#tablaTarifas").append("<tr>");
+	$("#tablaTarifas").append("<td>Masaje descontracturante</td>");
+	$("#tablaTarifas").append("<td>1 hora</td>");
+	$("#tablaTarifas").append("<td>$400</td>");
+	$("#tablaTarifas").append("</tr>");
+}
+
+function cargarDatosFooter(){
+
+	$(".footer-left").append("<img src='Imagenes_vero/candle01.jpg' id='logoImg'>");
+	$(".fa fa-map-marker").append("<p><span>A domicilio - Solo capital</span><span>En morón consultorio particular</span></p>");
+	$(".fa fa-phone").append("<p>11 6119-9549</p>");
+	$(".fa fa-envelope").append("<p><a href='mailto:verin_ ajf@hotmail.com'>verin_ ajf@hotmail.com</a></p>");
+	$(".footer-company-about").append("<span>Otros medios de contacto</span>");
+	$(".footer-icons").append("<p id='footer_instagram'>Instagram</p>");
+	$(".footer-icons").append("<a href='https://www.instagram.com/masoterapeuta1202/'><i class='fa fa-instagram'></i></a>");
+}
+
+function agregarBoxShadow(){
+	document.getElementById("infoDoctor").style.boxShadow = "5px 5px 5px 0px rgba(0,0,0,0.35)";
+	document.getElementById("encabezado").style.boxShadow = "5px 5px 5px 0px rgba(0,0,0,0.35)";
+	document.getElementById("solicitarTurno").style.boxShadow = "5px 5px 5px 0px rgba(0,0,0,0.35)";
+}
+
+function mostrarHora(){
+	var hora;
+	var minutos;
+	var segundos;
+	var miHora = new Date();
+
+	 hora 	  = miHora.getHours();
+	 minutos  = miHora.getMinutes();
+	 segundos = miHora.getSeconds(); 
+	 var imp  = hora+":"+minutos+" Hrs.";
+	 $("#horaActual").append(imp);
+}
+
+function borrarHora(){
+	var hora = document.getElementById("horaActual");
+	hora.parentNode.removeChild(hora);
+}
+
+function detectarResolucion() {
+var ancho = screen.width; // ancho de la pantalla del usuario
+var alto  = screen.height; // alto de la pantalla del usuario
+var pagina;
+
+if(ancho <= 800){
+	pagina="Videos/Video.html";
+}
+else if(ancho > 800 && ancho < 1200){
+	pagina="Videos/Video.html";
+}
+else{
+	pagina="Videos/Video.html";
+}
+
+//tiempoBanner(pagina);
+
+}
+
+function tiempoBanner(pagina){
+	setTimeout(abrirBanner,3000,pagina);
+}
+
+
+function abrirBanner(pagina){
+	window.open(pagina, '','width=1100,height=480');
+}
+
+
+
+function eliminarBordes(){
+ $(window).resize(function() {
+                        $windowWidth = $(window).width();
+                            $windowHeight = $(window).height();
+                        });
+
+ $(document).ready(function() {
+      //same logic that you use in the resize...
+  })
+
+//$("div.cont_lineas_izq" ).remove();
+
+
+
+
+/*var parent  = document.getElementByTagName("div");
+var element = document.getElementByClassName("cont_lineas_izq");
+parent.removeChild(element);
+
+
+var parent  = document.getElementByTagName("div");
+var element = document.getElementByClassName("cont_lineas_der");
+parent.removeChild(element);*/
+
+}
